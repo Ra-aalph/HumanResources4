1b7b3cc361afed4862b8889a2b7d794b55856b64
Emjhay Dela Cruz <emjhayd7@gmail.com>
2024-10-17 17:16:41 +0800

    updated

--- server/model/Benefits.js
+++ server/model/Benefits.js
@@ -9,7 +9,7 @@ const BenefitsSchema = new mongoose.Schema({
   philHealth: { type: Boolean, default: false },
   retirement: { type: Boolean, default: false },
   thirteenthMonth: { type: Boolean, default: false },
-});
+},  { timestamps: true });
 
 const Benefits = mongoose.model('Benefits', BenefitsSchema);
 module.exports = Benefits;
