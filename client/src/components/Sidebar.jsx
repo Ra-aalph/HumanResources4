3853fce43d69522190da581030edf15aa3c8ef97
Emjhay Dela Cruz <emjhayd7@gmail.com>
2024-10-17 17:06:28 +0800

    updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import GridViewIcon from "@mui/icons-material/GridView"; // Import icons
 import AccessTimeIcon from "@mui/icons-material/AccessTime"; // Import icons
 import RedeemIcon from "@mui/icons-material/Redeem"; // Import icons
